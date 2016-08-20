5413acaeda11cf0c93638f992eebece68d326e47
hp <hp@HP-PC>
2016-08-20 14:22:26 +0800

    刷图时下一屏变黑

--- Spectra/Windows/DynamicImagingWindow_Win32.cs
+++ Spectra/Windows/DynamicImagingWindow_Win32.cs
@@ -49,6 +49,14 @@ namespace Spectra
 
         internal void Update(byte[] buf_Dynamic, ushort frameCount, byte chanel)
         {
+            if (frameCount % 1000 == 0)
+            {
+                Parallel.For(0, 1000, r => {
+                    Parallel.For(0, 512, c => {
+                        buffer_img[r * 2048 * 3+ (chanel - 1) * 3 * 512 + c * 3] = buffer_img[r * 2048 * 3+ (chanel - 1) * 3 * 512 + c * 3 + 1] = buffer_img[r * 2048 * 3+ (chanel - 1) * 3 * 512 + c * 3 + 2] = 0;
+                    });
+                });
+            }
             Parallel.For(0, 512, i =>
             {
                 //buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 2] = buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 1] = buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 0] = (byte)(Math.Floor(((double)DataProc.readU16_PIC(buf_Dynamic, i * 2) / 4096 * 256)));
