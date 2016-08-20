a3362af8c15030ca6f8c00fb59dc4d79915dad3f
Levikov <leevick.zju@foxmail.com>
2016-08-20 14:23:17 +0800

    改进：刷图性能

--- Spectra/Windows/DynamicImagingWindow_Win32.cs
+++ Spectra/Windows/DynamicImagingWindow_Win32.cs
@@ -38,7 +38,8 @@ namespace Spectra
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Marshal.Copy(buffer_img, 0, test_data.Scan0, 2048 * 1000 * 3);
+            //Marshal.Copy(buffer_img, 0, test_data.Scan0, 2048 * 1000 * 3);
+ 
             pictureBox1.Image = new Bitmap(2048, 1000, 6144, PixelFormat.Format24bppRgb, test_data.Scan0);
         }
 
@@ -49,13 +50,18 @@ namespace Spectra
 
         internal void Update(byte[] buf_Dynamic, ushort frameCount, byte chanel)
         {
+            byte[] buffer_temp = new byte[1536];
             Parallel.For(0, 512, i =>
             {
                 //buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 2] = buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 1] = buffer_img[(frameCount - 34152) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 0] = (byte)(Math.Floor(((double)DataProc.readU16_PIC(buf_Dynamic, i * 2) / 4096 * 256)));
-                buffer_img[(frameCount%1000) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 2] = buffer_img[(frameCount%1000) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 1] = buffer_img[(frameCount%1000) * 2048 * 3 + (chanel - 1) * 3 * 512 + i * 3 + 0] = (byte)(Math.Floor(((double)DataProc.readU16_PIC(buf_Dynamic, i * 2) / 4096 * 256)));
-
+                buffer_temp[i * 3 + 2] = buffer_temp[i * 3 + 1] = buffer_temp[i * 3 + 0] = (byte)(Math.Floor(((double)DataProc.readU16_PIC(buf_Dynamic, i * 2) / 4096 * 256)));
+                
             });
-            
+            Marshal.Copy(buffer_temp,0, test_data.Scan0+(frameCount%1000)*2048*3+512*(chanel-1)*3, 1536);
+            if (frameCount % 1000 == 999&&chanel==4)
+            {
+                Marshal.Copy(buffer_img, 0, test_data.Scan0, 3 * 2048 * 1000);
+            }
         }
     }
 }
