e6fb2b00af78913f4431ff990c5fb910eac8fe56
Victo <Victo@VICTOR-ASUS>
2016-09-08 23:23:53 +0800

    修改：Map控件

--- Spectra/UserControls/Ctrl_Map.xaml.cs
+++ Spectra/UserControls/Ctrl_Map.xaml.cs
@@ -38,6 +38,11 @@ namespace Spectra
             MapControl.ZoomToFullEnvelope();
             MapControl.Refresh();
         }
+
+        internal void Refresh()
+        {
+            
+        }
     }
     public static class MapTilerSample
     {
